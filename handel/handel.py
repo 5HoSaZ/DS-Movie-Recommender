2611ebe6781c327bd4e5d9b98bc6afc971a0399f
Paul Jesus <hoangminhquan172003@gmail.com>
2024-11-10 21:03:26 +0700

    redo

--- handel/handel.py
+++ handel/handel.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 from pathlib import Path
+import ast
 
-file_path = Path("imdb-movie-scraper\database\movie_entries.csv")
+file_path = Path("imdb-movie-scraper\database\movie_entries 2.csv")
 if file_path.exists():
     data = pd.read_csv(file_path)
     print('sucess')
@@ -10,23 +11,31 @@ else:
     print(f"File not found: {file_path}")
 
 
-print(data.head())
+
 
 # recommendation will be based on these features only
 data = data.loc[:,['ImdbID', 'Name', 'Directors',
-       'OriginCountries', 'Languages', 'Genres', 'Rating']]
+       'OriginCountries', 'Languages', 'Genres', 'Rating', 'Plot', 'Cast']]
+
+
 
 data['Directors'] = data['Directors'].replace(np.nan, 'unknown')
 data['Languages'] = data['Languages'].replace(np.nan, 'unknown')
 data['OriginCountries'] = data['OriginCountries'].replace(np.nan, 'unknown')
+data['Plot'] = data['Plot'].replace(np.nan, 'unknown')
+data['Cast'] = data['Cast'].replace(np.nan, 'unknown')
+
 
 
 data['Directors'] = data['Directors'].str.replace('[', ' ').str.replace(']', ' ').str.replace("'", "")
 data['OriginCountries'] = data['OriginCountries'].str.replace('[', ' ').str.replace(']', ' ').str.replace("'", "")
 data['Languages'] = data['Languages'].str.replace('[', ' ').str.replace(']', ' ').str.replace("'", "")
 data['Genres'] = data['Genres'].str.replace('[', ' ').str.replace(']', ' ').str.replace("'", "").str.replace(',', '')
+data['Cast'] = data['Cast'].str.replace('[', ' ').str.replace(']', ' ').str.replace("'", "").str.replace(',', '')
 
-import ast
+
+
+
 data['Genres'] = data['Genres'].map(lambda x: ast.literal_eval(x) if isinstance(x, str) and x.startswith('[') else x)
 
 #Now we will have to take out the individual genres like adventure, action, sci-fi, etc. using the following function
@@ -46,6 +55,7 @@ def make_genresList(x):
     else:
         return (st.join(gen)) #then we will join them together and return the valuse
     
-
+print(data.head())
+print(data.dtypes)
 
 data.to_csv('imdb-movie-scraper\database\handel1.csv',index=False)
